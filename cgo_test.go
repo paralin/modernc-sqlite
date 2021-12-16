810a77a6c8712c1d898e793c056c9e1e9450078d
glebarez <gleb.sakhnov@gmail.com>
2021-12-16 13:01:29 +0100

    adjust build contstraint

--- cgo_test.go
+++ cgo_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (cgo && cgobench) || ignore || (cgo && cgotest)
-// +build cgo,cgobench ignore cgo,cgotest
+//go:build ignore || (cgo && cgotest)
+// +build ignore cgo,cgotest
 
 package sqlite // import "modernc.org/sqlite"
 import (
